03fb001e5e8e28fa5e32510614347d115a1b38b3
Gu sir <390512308@qq.com>
2017-03-10 10:13:42 +0800

    modify constructor

--- python/image/base.py
+++ python/image/base.py
@@ -1,8 +1,8 @@
 from .. import lib
 
 
-def pyramid(*args, **kwargs): return lib.tl.pyramid(*args, **kwargs)
+def pyramid(*args, **kwargs): return lib.pyramid(*args, **kwargs)
 
-def assign_image(*args, **kwargs): return lib.tl.assign_image(*args, **kwargs)
+def assign_image(*args, **kwargs): return lib.assign_image(*args, **kwargs)
 
-def flat_color(*args, **kwargs): return lib.tl.flat_color(*args, **kwargs)
+def flat_color(*args, **kwargs): return lib.flat_color(*args, **kwargs)
--- python/lib.py
+++ python/lib.py
@@ -1,12 +1,15 @@
 
 import os
+import inspect
 import tensorflow as tf
 
 module_path = os.path.dirname(__file__)
 tl = tf.load_op_library(os.path.join(module_path, "libtensorlab.so"))
 
-#def pyramid(*args, **kwargs): return tl.pyramid(*args, **kwargs)
+def_function = "def {0}(*args, **kwargs): return tl.{0}(*args, **kwargs)"
 
-#def assign_image(*args, **kwargs): return tl.assign_image(*args, **kwargs)
+for name in dir(tl):
+    f = getattr(tl, name)
+    if not inspect.isfunction(f): continue
+    exec(def_function.format(name))
 
-#def flat_color(*args, **kwargs): return tl.flat_color(*args, **kwargs)
