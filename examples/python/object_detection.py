add batch normalization and fix random crop

examples/python/object_detection.py
@@ -1,55 +1,91 @@
+import math
+import cv
 import tensorflow as tf
 import numpy as np
 import tensorlab as tl
-from tensorlab.framework import *
-import math
-import cv
+from tensorlab.ops import batch_normalization as bn
 from util import *
 from support import dataset
 from support.image import RandomCrop
+import time
+
+def load_data(file):
+    def create_rectangle(t, l, w, h):
+        p = tl.Point2f(t, l)
+        return tl.Rectanglef.create_with_tlwh(p, w, h)
+
+    images, labels = dataset.load_object_detection_xml(file, create_rectangle)
+    return images, labels
 
 
 
-def rotate_about_center(src, angle, scale=1.):
-    w = src.shape[1]
-    h = src.shape[0]
-    rangle = np.deg2rad(angle)  # angle in radians
-    # now calculate new image width and height
-    nw = (abs(np.sin(rangle)*h) + abs(np.cos(rangle)*w))*scale
-    nh = (abs(np.cos(rangle)*h) + abs(np.sin(rangle)*w))*scale
-    # ask OpenCV for the rotation matrix
-    rot_mat = cv2.getRotationMatrix2D((nw*0.5, nh*0.5), angle, scale)
-    # calculate the move from the old center to the new center combined
-    # with the rotation
-    rot_move = np.dot(rot_mat, np.array([(nw-w)*0.5, (nh-h)*0.5,0]))
-    # the move only affects the translation, so update the translation
-    # part of the transform
-    rot_mat[0,2] += rot_move[0]
-    rot_mat[1,2] += rot_move[1]
-    return cv2.warpAffine(src, rot_mat, (int(math.ceil(nw)), int(math.ceil(nh))), flags=cv2.INTER_LANCZOS4)
+def create_model(input):
+    def weight(shape):
+        initial = tf.truncated_normal(shape, stddev=0.1)
+        return tf.Variable(initial)
+
+    def conv2d(x, W, strides, padding):
+        return tf.nn.conv2d(x, W, strides=strides, padding=padding)
+
+    tensor = input
+    tensor = conv2d(tensor, weight([5, 5, 3, 32]), [1, 2, 2, 1], padding="VALID")
+    tensor = conv2d(tensor, weight([5, 5, 32, 32]), [1, 2, 2, 1], padding="VALID")
+    tensor = conv2d(tensor, weight([5, 5, 32, 32]), [1, 2, 2, 1], padding="VALID")
+
+    tensor = conv2d(tensor, weight([3, 3, 32, 32]), [1, 1, 1, 1], padding="SAME")
+    tensor = conv2d(tensor, weight([3, 3, 32, 32]), [1, 1, 1, 1], padding="SAME")
+    tensor = conv2d(tensor, weight([3, 3, 32, 32]), [1, 1, 1, 1], padding="SAME")
+
+    tensor = conv2d(tensor, weight([6, 6, 32, 1]), [1, 1, 1, 1], padding="SAME")
+
+    return tensor
 
 
-def create_rectangle(t, l, w, h):
-    p = tl.Point2f(t, l)
-    return tl.Rectanglef.create_with_tlwh(p, w, h)
-images, labels = dataset.load_object_detection_xml("../data/testing.xml", create_rectangle)
 
-rand_crop = RandomCrop((200, 200))
-images_tensor, image_rects = rand_crop(images, labels, 100)
-
-with tf.Session() as sess:
-    crop_images = images_tensor.eval()
-    print("crop_images.shape", crop_images.shape)
-
-    for i in xrange(len(crop_images)):
-        img = crop_images[i]
-        rects = image_rects[i]
+def debug_images_rects(sess, images_tensor, rects_list):
+    result = sess.run([images_tensor])
+    images = result[0]
+    for i in xrange(len(images)):
+        img = images[i]
+        rects = rects_list[i]
         for r in rects:
-            cv2.rectangle(img, (int(r.left), int(r.top)), (int(r.right), int(r.bottom)), color=(0, 0, 255), thickness=2)
-            print("rect area:", r.area)
-        cv2.imshow("crop", img)
+            cv2.rectangle(img, (int(r.left), int(r.top)), (int(r.right), int(r.bottom)), color=(0, 0, 255),
+                          thickness=2)
 
-        #cv2.imshow("origin", images[0])
-        print("progress {0}/{1} rect:{2}".format(i+1, len(crop_images), len(rects)))
+        cv2.imshow("image", img)
+        print("progress {0}/{1} rect:{2}".format(i + 1, len(images), len(rects)))
         press_key_stop()
+
+
+def main():
+    crop_size = (200, 200)
+    crop_per_image = 30
+    pyramid_scale = 6
+
+    # load train datas
+    images, labels = load_data("../data/training.xml")
+
+    # create crop generator
+    croper = RandomCrop(crop_size)
+
+    # create model
+    input = tf.placeholder(tf.float32, (None, None, None, None))
+    model = create_model(input)
+
+
+    # train
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+
+    while True:
+        mini_batch_samples, mini_batch_labels = croper(images, labels, crop_per_image)
+        mini_batch_samples = tl.image.pyramid(mini_batch_samples, pyramid_scale)
+        debug_images_rects(sess, mini_batch_samples, mini_batch_labels)
+        exit()
+
+    sess.close()
+
+
+if __name__ == "__main__":
+    main()
 

examples/python/support/image/random_crop.py
@@ -30,18 +30,34 @@ class RandomCrop(object):
 
 
     def __call__(self, images, labels, crops_per_image):
-        image = images[0]
-        rects = labels[0]
+        assert len(images) == len(labels)
 
-        plans = []
-        for i in xrange(crops_per_image):
-            plan = self._make_plan(image.shape, rects)
-            plans.append(plan)
+        # gen tensors and rects for all images
+        crop_tensor_list = []
+        crop_rects_list = []
+        for i in xrange(len(images)):
+            image = images[i]
+            rects = labels[i]
 
-        crops_tensor = self.gen_extract_image_chip_tensor(image, plans)
-        crop_rects = self.gen_crop_image_rects(image.shape, rects, plans)
+            plans = []
+            for i in xrange(crops_per_image):
+                plan = self._make_plan(image.shape, rects)
+                plans.append(plan)
 
-        return crops_tensor, crop_rects
+            crop_tensor_list += self.gen_extract_image_chip_tensor(image, plans)
+            crop_rects_list += self.gen_crop_image_rects(image.shape, rects, plans)
+
+        # shuffle and concat tensor
+        index = range(len(crop_tensor_list))
+        np.random.shuffle(index)
+        crop_tensor = None
+        crop_rects = []
+        for i in index:
+            t = crop_tensor_list[i]
+            crop_tensor = t if crop_tensor is None else tf.concat([crop_tensor, t], 0)
+            crop_rects.append(crop_rects_list[i])
+
+        return crop_tensor, crop_rects
 
 
     def gen_extract_image_chip_tensor(self, image, plans):
@@ -73,20 +89,17 @@ class RandomCrop(object):
 
         # flip
         chip_r, chip_c = self._chips_dims
-        flip_tensor = None
+        flip_tensors = []
         for i in xrange(len(plans)):
             plan = plans[i]
             tensor = crops_tensor[i]
             if plan.flip:
                 tensor = tf.image.flip_left_right(tensor)
+
             tensor = tf.reshape(tensor, (1, chip_r, chip_c, d))
+            flip_tensors.append(tensor)
 
-            if flip_tensor is None:
-                flip_tensor = tensor
-            else:
-                flip_tensor = tf.concat([flip_tensor, tensor], 0)
-
-        return flip_tensor
+        return flip_tensors
 
 
     def gen_crop_image_rects(self, image_shape, rects, plans):

examples/python/test.py
@@ -10,148 +10,19 @@ import cv
 from util import *
 import threading
 import time
+from tensorflow.python.ops import control_flow_ops
 
 images, labels = dataset.load_object_detection_xml("../data/testing.xml")
 
-images = np.array([images[0], images[0], images[0], images[0], images[0]])
+tcond = tf.Variable(True, dtype=tf.bool)
 
-image = np.array([images[0]])
-simage = images[0]
+c1 = tf.constant(1)
+c2 = tf.constant(0)
 
-a = np.array([[1,2,3], [4,5,6], [7,8,9]])
-
-
-def gen_transform_mat(image_shape, anchor, translate=[0, 0], angle=0, scale=[1, 1]):
-    anchor = np.array(anchor, dtype=np.float32)
-    translate = np.array(translate, dtype=np.float32)
-    scale = np.array(scale, dtype=np.float32)
-
-    r, c, d = image_shape
-    radian = np.deg2rad(angle)
-    r_mat = np.array([[np.cos(radian), np.sin(radian)],
-                      [-np.sin(radian), np.cos(radian)]], dtype=np.float32)
-
-    r_mat[0, 0] /= scale[0]
-    r_mat[1, 1] /= scale[1]
-
-    t_vec = np.dot(r_mat, anchor * -1) + anchor + np.array(translate)
-    mat = np.column_stack((r_mat, t_vec))
-    mat = np.array([list(mat[0]) + list(mat[1]) + [0] * 2], dtype=np.float32)
-
-    return mat
-
-b, r, c, d = image.shape
-mat = gen_transform_mat((r, c, d), (c/2, r/2), angle=30)
-tensor = tf.contrib.image.transform(image, mat)
+cond = control_flow_ops.cond(tcond, lambda : c1, lambda : c2)
 
 with tf.Session() as sess:
-    new_image = tensor.eval()[0]
-    cv2.imshow("crop", new_image)
+    print(c1.name)
+    print(c2.name)
 
 
-mat = mat[0]
-r_mat = np.array([mat[[0,1]], mat[[3,4]]])
-t_vec = mat[[2,5]]
-
-new_image = np.zeros_like(simage)
-
-for i in xrange(r):
-    for j in xrange(c):
-        point = np.array([j, i])
-        new_point = np.dot(r_mat, point) + t_vec
-        if new_point[0] < 0 or new_point[0] > c: continue
-        if new_point[1] < 0 or new_point[1] > r: continue
-        nx, ny = new_point[0], new_point[1]
-        new_image[ny, nx] = simage[i, j]
-
-cv2.imshow("mine", new_image)
-
-
-
-rot_mat = cv2.getRotationMatrix2D((c/2, r/2), -30, 1)
-rot_mat = np.column_stack((r_mat, t_vec))
-new_image = cv2.warpAffine(simage, rot_mat, (c, r), flags=cv2.INTER_LANCZOS4)
-cv2.imshow("cv", new_image)
-
-
-press_key_stop()
-
-'''
-exit_flag = False
-
-# Thread body: loop until the coordinator indicates a stop was requested.
-# If some condition becomes true, ask the coordinator to stop.
-def MyLoop(coord, index):
-    starttime = time.time()
-    last_time = time.time()
-    while not coord.should_stop():
-        curtime = time.time()
-        if curtime - last_time > 3:
-            print("thread {0} works".format(index))
-            last_time = curtime
-
-        for i in xrange(1000000):
-            t = index / 3
-
-        if curtime - starttime > 30:
-            print("request stop")
-            coord.request_stop()
-
-# Main thread: create a coordinator.
-coord = tf.train.Coordinator()
-
-# Create 10 threads that run 'MyLoop()'
-threads = [threading.Thread(target=MyLoop, args=(coord, i)) for i in xrange(100)]
-
-# Start the threads and wait for all of them to stop.
-for t in threads:
-  t.start()
-coord.join(threads)
-
-
-
-exit()
-'''
-
-'''
-model = Model()
-x1 = model.add_input(tf.int32, [None, None], name="x1")
-x2 = model.add_input(tf.int32, [None, None])
-res = Layer(tf.add)
-
-x1.adds([
-    Layer(tf.multiply, y=2),
-    Layer(tf.add, y=3),
-])
-
-x2.adds([
-    Layer(tf.div, y=2),
-    Layer(tf.add, y=-7),
-])
-
-
-res.adds([
-    Layer(tf.cast, dtype=tf.float32),
-    Layer(tf.log),
-])
-
-x1.tail.add(res)
-x2.tail.add(res)
-
-model.summary()
-
-with tf.Session() as sess:
-    x = np.random.randint(1, 10, size=(3, 3))
-    y = np.random.randint(1, 10, size=(3, 3))
-
-    x = np.array([[1, 1, 1], [2, 2, 2]])
-    y = np.array([[3, 3, 3], [4, 4, 4]])
-
-    result = model.run(sess, feed_dict = {"x1": x, x2: y})
-
-    for k, v in result.items():
-        print(k)
-        print(v)
-        print('-' * 10)
-
-'''