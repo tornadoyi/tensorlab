87e78983090106e49b626836b6e556fe8e9d97c8
Gu sir <390512308@qq.com>
2017-03-26 15:32:51 +0800

    construct cnn for test

--- examples/python/object_detection.py
+++ examples/python/object_detection.py
@@ -3,7 +3,6 @@ import cv
 import tensorflow as tf
 import numpy as np
 import tensorlab as tl
-from tensorlab.ops import batch_normalization as bn
 from util import *
 from support import dataset
 from support.image import RandomCrop
@@ -19,24 +18,47 @@ def load_data(file):
 
 
 
-def create_model(input):
+def create_model(input, is_training):
     def weight(shape):
         initial = tf.truncated_normal(shape, stddev=0.1)
         return tf.Variable(initial)
 
-    def conv2d(x, W, strides, padding):
-        return tf.nn.conv2d(x, W, strides=strides, padding=padding)
+    def bias(shape):
+        initial = tf.constant(0.1, shape=shape)
+        return tf.Variable(initial)
+
+    def conv2d(x, W, b, strides, padding):
+        return tf.nn.conv2d(x, W, strides=strides, padding=padding) + b
+
+    def bn(inputs, is_traning):
+        return tf.layers.batch_normalization(inputs, training=is_traning)
 
     tensor = input
-    tensor = conv2d(tensor, weight([5, 5, 3, 32]), [1, 2, 2, 1], padding="VALID")
-    tensor = conv2d(tensor, weight([5, 5, 32, 32]), [1, 2, 2, 1], padding="VALID")
-    tensor = conv2d(tensor, weight([5, 5, 32, 32]), [1, 2, 2, 1], padding="VALID")
+    tensor = conv2d(tensor, weight([5, 5, 3, 32]), bias([32]), [1, 2, 2, 1], padding="VALID")
+    tensor = bn(tensor, is_training)
+    tensor = tf.nn.relu(tensor)
 
-    tensor = conv2d(tensor, weight([3, 3, 32, 32]), [1, 1, 1, 1], padding="SAME")
-    tensor = conv2d(tensor, weight([3, 3, 32, 32]), [1, 1, 1, 1], padding="SAME")
-    tensor = conv2d(tensor, weight([3, 3, 32, 32]), [1, 1, 1, 1], padding="SAME")
+    tensor = conv2d(tensor, weight([5, 5, 32, 32]), bias([32]), [1, 2, 2, 1], padding="VALID")
+    tensor = bn(tensor, is_training)
+    tensor = tf.nn.relu(tensor)
 
-    tensor = conv2d(tensor, weight([6, 6, 32, 1]), [1, 1, 1, 1], padding="SAME")
+    tensor = conv2d(tensor, weight([5, 5, 32, 32]), bias([32]), [1, 2, 2, 1], padding="VALID")
+    tensor = bn(tensor, is_training)
+    tensor = tf.nn.relu(tensor)
+
+    tensor = conv2d(tensor, weight([3, 3, 32, 32]), bias([32]), [1, 1, 1, 1], padding="SAME")
+    tensor = bn(tensor, is_training)
+    tensor = tf.nn.relu(tensor)
+
+    tensor = conv2d(tensor, weight([3, 3, 32, 32]), bias([32]), [1, 1, 1, 1], padding="SAME")
+    tensor = bn(tensor, is_training)
+    tensor = tf.nn.relu(tensor)
+
+    tensor = conv2d(tensor, weight([3, 3, 32, 32]), bias([32]), [1, 1, 1, 1], padding="SAME")
+    tensor = bn(tensor, is_training)
+    tensor = tf.nn.relu(tensor)
+
+    tensor = conv2d(tensor, weight([6, 6, 32, 1]), bias([1]), [1, 1, 1, 1], padding="SAME")
 
     return tensor
 
@@ -70,18 +92,29 @@ def main():
 
     # create model
     input = tf.placeholder(tf.float32, (None, None, None, None))
-    model = create_model(input)
+    is_training = tf.Variable(False, dtype=tf.bool)
+    model = create_model(input, is_training)
 
 
     # train
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
+    train_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+    update_vars = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
 
     while True:
+        set_is_training = tf.assign(is_training, True)
+        is_train = sess.run([set_is_training, is_training])[1]
+        print(is_training)
+
         mini_batch_samples, mini_batch_labels = croper(images, labels, crop_per_image)
         mini_batch_samples = tl.image.pyramid(mini_batch_samples, pyramid_scale)
-        debug_images_rects(sess, mini_batch_samples, mini_batch_labels)
+        #debug_images_rects(sess, mini_batch_samples, mini_batch_labels)
+
+        mini_batch_samples = sess.run([mini_batch_samples])[0]
+        sess.run([model]+update_vars, feed_dict={input: mini_batch_samples})
         exit()
+
 
     sess.close()
 
