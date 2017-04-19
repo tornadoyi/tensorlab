c7d3f3ffe8e0d665466608081f921327262fd55b
Gu sir <390512308@qq.com>
2017-04-19 11:37:43 +0800

    add trainer

--- examples/python/test.py
+++ examples/python/test.py
@@ -25,21 +25,34 @@ images, labels = dataset.load_object_detection_xml("../data/testing.xml")
 
 sess = tf.InteractiveSession()
 
+gloabl_i = 1
 def gen_samples():
+    global gloabl_i
     w = np.array([4,3,2,1], np.float32)
-    x = np.random.randint(0, 10, size=4)
+    x = np.array([1,1,1,1]) * gloabl_i#np.random.randint(0, 10, size=4)
     y = w * x
     return x, y
 
 x = tf.placeholder(tf.int32, [None, ])
 y = tf.placeholder(tf.int32, [None, ])
 
-w = tf.Variable([1,2,3,4], dtype=tf.float32)
+w = tf.Variable([1,2,3,4], dtype=tf.float32, name="vv")
 y_ = w * tf.to_float(x)
 
 loss = tf.nn.l2_loss(tf.to_float(y) - y_)
 
 train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
+def gen_feed_dict():
+    sx, sy = gen_samples()
+    return {x: sx, y:sy}
 
-trainer = Trainer(sess, )
+trainer = Trainer(sess, checkpoint="checkpoint/test/test.ckpt", max_epoch=10, max_save_epoch=5)
+
+
+def step_call_back(r):
+    print(trainer.epoch)
+    print("loss", r[1])
+
+
+trainer([train_step, loss], gen_feed_dict, step_call_back)
--- python/framework/trainer.py
+++ python/framework/trainer.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
 import time
+import os
+from types import FunctionType, MethodType
+import pickle
 
 
 class Trainer(object):
@@ -7,15 +10,19 @@ class Trainer(object):
                  sess,
                  init_variables=True,
                  checkpoint=None,
-                 max_save_second = 60,
+                 max_save_second = None,
+                 max_save_epoch = None,
                  max_epoch = None):
         self._sess = sess
         self._init_variables = init_variables
         self._checkpoint = checkpoint
         self._max_save_second = max_save_second
+        self._max_save_epoch = max_save_epoch
         self._max_epoch = max_epoch
         self._saver = tf.train.Saver()
-        self._epoch = tf.Variable(0, dtype=tf.int32, trainable=False)
+
+        # state information
+        self._epoch = 0
 
 
     def __call__(self, fetches, feed_dict=None, epoch_call=None):
@@ -24,30 +31,90 @@ class Trainer(object):
         if self._init_variables: self._sess.run(tf.global_variables_initializer())
 
         # restore checkpoint
-        if self._checkpoint is not None: self._saver.restore(self._sess, self._checkpoint)
+        self.load_checkpoint()
 
         # temp vars
         pre_save_time = time.time()
 
         # train
         while True:
+            # epoch +1
+            self._epoch += 1
+
+            # end by epoch
+            if self._max_epoch is not None and self._epoch > self._max_epoch: break
+
+            # get feed dict
+            feed_dict_data = feed_dict
+            if isinstance(feed_dict, FunctionType) or isinstance(feed_dict, MethodType):
+                feed_dict_data = feed_dict()
+
             # run
-            result = self._sess.run(fetches, feed_dict)
+            result = self._sess.run(fetches, feed_dict_data)
 
             # time
             curtime = time.time()
 
             # save checkpoint
-            if self._checkpoint is not None and curtime - pre_save_time > self._max_save_second:
+            if (self._checkpoint is not None and curtime - pre_save_time >= self._max_save_second) or \
+                (self._max_save_epoch is not None and self._epoch >= self._max_save_epoch):
                 pre_save_time = curtime
-                self._saver.save(self._sess, self._checkpoint)
+                self.save_checkpoint()
 
             # call back
             if epoch_call is not None: epoch_call(result)
 
 
-            # epoch +1
-            self._epoch.assign_add(1)
 
-            # end by epoch
-            if self._max_epoch is not None and self._epoch.eval() >= self._max_epoch: break
+
+    @property
+    def epoch(self): return self._epoch
+
+
+    def load_checkpoint(self):
+        if self._checkpoint is None: return
+
+        # check dir valid
+        dirname = os.path.dirname(self._checkpoint)
+        if not os.path.isdir(dirname): return
+
+        # check checkpoint files
+        filename = os.path.basename(self._checkpoint)
+        filelist = ["checkpoint", filename + ".data", filename + ".index", filename + ".meta", filename + ".state"]
+        match = 0
+        for name in os.listdir(dirname):
+            for fn in filelist:
+                if name.find(fn) < 0: continue
+                match += 1
+                break
+
+        if match < len(filelist): return
+        try:
+            self._saver.restore(self._sess, self._checkpoint)
+            state_file_path = self._checkpoint + ".state"
+            with open(state_file_path) as f:
+                state = pickle.loads(f.read())
+                self._epoch = state["epoch"]
+        except Exception, e:
+            print(e)
+
+
+    def save_checkpoint(self):
+        if self._checkpoint is None: return
+        dirname = os.path.dirname(self._checkpoint)
+        if not os.path.isdir(dirname): os.makedirs(dirname)
+
+        try:
+            # save checkpoint
+            self._saver.save(self._sess, self._checkpoint)
+
+            # save state
+            state = {
+                "epoch": self._epoch
+            }
+            state_file_path = self._checkpoint + ".state"
+            with open(state_file_path, "w") as f: f.write(pickle.dumps(state))
+
+
+        except Exception,e:
+            print(e)
